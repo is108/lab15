2ea169124e7c8c899909682f9d8a7dfe76600448
is108 <alexey21s@rambler.ru>
2020-06-13 22:47:51 +0700

    добавлена лабораторная по имитационному

---
+++ laba_15/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace laba_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 119);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(168, 47);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(233, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.LabelBorderWidth = 3;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(568, 379);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(62, 207);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(107, 39);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop/ Cont.";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(34, 530);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(165, 47);
+            this.btnCompare.TabIndex = 3;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(233, 397);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.IsValueShownAsLabel = true;
+            series2.IsXValueIndexed = true;
+            series2.LabelFormat = "f3";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series2";
+            this.chart2.Series.Add(series2);
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(568, 408);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 823);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ laba_15/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_15
+{
+    public partial class Form1 : Form
+    {
+        MathStatistic stat;
+        int state; 
+        Random rnd = new Random();
+        double a, time;
+        double[] Q;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stat = new MathStatistic();
+            Q = new double[3] { -0.4, -0.8, -0.5 };
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time = 0;
+            state = 0;
+            chart1.Series[0].Points.Clear();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.AddXY((int)time, state + 1);
+            a = rnd.NextDouble();
+            time += Math.Log(a) / Q[state];
+            state = stat.Modeling(a);
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        // сравнение эмпирического распределения вероятностей с теоретическим финальным
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            stat.Statistic();
+            for(int i = 0; i < 3; i++)
+            {
+                chart2.Series[0].Points.AddXY(i + 1, stat.Frequency[i]);
+                chart2.Series[1].Points.AddXY(i + 1, stat.Final[i]);
+            }
+        }
+
+        class MathStatistic
+        {
+            // матрица интенсивностей переходов
+            double[,] Q = new double[3, 3] { { -0.4, 0.3, 0.1 },
+                                             { 0.4, -0.8, 0.4 },
+                                             { 0.1, 0.4, -0.5 } };
+            int N = 1000;
+            int T = 1000;
+            int state = 0, k; 
+            Random rnd = new Random();
+            double a, p;
+            public double[] Frequency = new double[3]; 
+            public double[] Final = new double[3]; 
+
+            public int Modeling(double a) 
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    // новое состояние - дискретная случайная величина с рядом распределения
+                    if (state != i)
+                    {
+                        a -= Q[state, i] / Math.Abs(Q[state, state]);
+                    }
+                    else a -= 0;
+
+                    if (a < 0)
+                    {
+                        state = i;
+                        return state;
+                    }
+                }
+                return state;
+            }
+            
+            public void Statistic()
+            {
+                state = 0;
+                for(int i = 0; i < 3; i++)
+                {
+                    Frequency[i] = 0;
+                    Final[i] = 0;
+                }
+
+                TheoreticalProbability();
+                StatisticalProcessing();
+            }
+
+            // статистическая обработка
+            public void StatisticalProcessing()
+            {
+                k = 0;
+                if (k < N)
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (double j = 0; j < T;)
+                        {
+                            a = rnd.NextDouble();
+                            NewStateGenerator();
+                            j += Math.Log(a) / Q[state, state];
+                        }
+                        Frequency[state] = Frequency[state] + 1;
+                        k++;
+                    }
+                }
+                // эмпирические распределения
+                for (int i = 0; i < 3; i++) Frequency[i] /= N;
+            }
+
+            public int NewStateGenerator()
+            {
+                for(int i  = 0; i < 3; i++)
+                {
+                    // новое состояние - дискретная случайная величина с рядом распределения
+                    if (state != i)
+                    {
+                        a -= Q[state, i] / Math.Abs(Q[state, state]);
+                    }
+                    else a -= 0;
+
+                    if(a < 0)
+                    {
+                        state = i;
+                        return state;
+                    }
+                }
+                return state;
+            }
+
+            // теоретическое финальное распределение
+            public void TheoreticalProbability()
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    /* Получаем эти коэффициенты решая систему уравнений
+                       Pi1(-0.4) + Pi2(0.3) + Pi3(0.1) = 0
+                       Pi1(0.3) + Pi2(-0.8) + Pi3(0.4) = 0
+                       Pi1 + Pi2 + Pi3 = 1
+                    */
+                    Final[0] = 0.37; 
+                    Final[1] = 0.31;
+                    Final[2] = 0.32;
+                }
+            }
+        }
+    }
+}
